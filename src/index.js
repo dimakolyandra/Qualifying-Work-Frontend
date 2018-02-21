c83ae5c413ce58f557ee2c458269589d9c1d3f63
Дмитрий Коляндра <dima.kolyandra@mail.ru>
2018-02-21 21:31:03 +0300

    Add carousel

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import ReactDom from 'react-dom';
+import React from 'react';
+
+import BaseCarousel from './containers/carousel'
+
+ReactDom.render(<BaseCarousel/>, document.getElementById('root'))
